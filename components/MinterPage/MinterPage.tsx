015cc4047c268c820f5d439e6036a2c14d4e508c
i9-9 <yvan.vrs@gmail.com>
2024-08-24 19:40:27 -0300

    Create, MintPage details on switch

--- components/MinterPage/MinterPage.tsx
+++ components/MinterPage/MinterPage.tsx
@@ -10,8 +10,8 @@ import MinterHeader from './MinterHeader'
 import SaleStrategySwitch from './SaleStrategySwitch'
 
 const MinterPage = () => (
-  <div className="flex min-h-[100dvh] flex-col items-center justify-center bg-background px-4 py-12 sm:px-6 lg:px-8">
-    <div className=" mt-2 gap-1 mx-auto max-w-md text-center items-center flex flex-col">
+  <div className="min-h-[100dvh] overflow-y-auto flex flex-col items-center justify-center bg-background px-4 py-12 sm:px-6 lg:px-8">
+    <div className=" mt-2 gap-[2px] mx-auto max-w-md text-center items-center flex flex-col">
       </div>
     <div className="gap-2 mx-auto max-w-md text-center items-center flex flex-col">
       <MinterHeader />
--- components/ui/Switch.tsx
+++ components/ui/Switch.tsx
@@ -8,23 +8,26 @@ const Switch = React.forwardRef<
   React.ElementRef<typeof SwitchPrimitives.Root>,
   React.ComponentPropsWithoutRef<typeof SwitchPrimitives.Root>
 >(({ className, ...props}, ref) => (
-  <div className="flex items-center space-x-2 font-ibmPlexMono">
-    <span className="text-sm font-medium text-[#D1F121] italic leading-none"> 111 </span>
-    <SwitchPrimitives.Root
-      className={cn(
-        "peer inline-flex h-6 w-11 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=unchecked]:bg-input",
-        className
-      )}
-      {...props}
-      ref={ref}
-    >
-      <SwitchPrimitives.Thumb
+  <div className="flex flex-col items-center space-y-1 font-ibmPlexMono">
+    <span className="text-xs font-medium text-[#D1F121]">Price</span>
+    <div className="flex items-center space-x-2">
+      <span className="text-sm font-medium text-[#D1F121] leading-none">✧ 111 </span>
+      <SwitchPrimitives.Root
         className={cn(
-          "pointer-events-none block h-5 w-5 rounded-full bg-background shadow-lg ring-0 transition-transform data-[state=checked]:translate-x-5 data-[state=unchecked]:translate-x-0"
+          "peer inline-flex h-6 w-11 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=unchecked]:bg-input",
+          className
         )}
-      />
-    </SwitchPrimitives.Root>
-    <span className="text-sm font-medium text-[#D1F121] italic leading-none">777 </span>
+        {...props}
+        ref={ref}
+      >
+        <SwitchPrimitives.Thumb
+          className={cn(
+            "pointer-events-none block h-5 w-5 rounded-full bg-background shadow-lg ring-0 transition-transform data-[state=checked]:translate-x-5 data-[state=unchecked]:translate-x-0"
+          )}
+        />
+      </SwitchPrimitives.Root>
+      <span className="text-sm font-medium text-[#D1F121] leading-none">777 ✧</span>
+    </div>
   </div>
 ))
 Switch.displayName = SwitchPrimitives.Root.displayName
