3aedc1e53702b42f6a249d08c90bba578ac31c85
i9-9 <yvan.vrs@gmail.com>
2024-08-24 17:25:36 -0300

    deleted landing page and fixes regarding last commit

--- components/MinterPage/Description.tsx
+++ components/MinterPage/Description.tsx
@@ -1,13 +1,11 @@
-import { useZoraCreateProvider } from '@/providers/ZoraCreateProvider'
 import { Textarea } from '../ui/Textarea'
 
 const Description = () => {
-  const { name, setName } = useZoraCreateProvider()
 
   return (
       <div className='flex flex-col'>
         <h3 className='font-ibmPlexMono text-[#D1F121] text-[9px] text-left'>Description</h3>
-        <Textarea value={name} onChange={(e) => setName(e.target.value)} />
+        <Textarea />
       </div>
 
   )
