0a4b93189b74b3dc119323b8d4622091d1be4a5b
Sweets Sweetman <sweetmantech@gmail.com>
2024-08-24 19:08:35 -0300

    I set 777 endTime to one month.

--- components/MinterPage/Description.tsx
+++ components/MinterPage/Description.tsx
@@ -1,13 +1,11 @@
 import { Textarea } from '../ui/Textarea'
 
 const Description = () => {
-
   return (
-      <div className='flex flex-col'>
-        <h3 className='font-ibmPlexMono text-[#D1F121] text-[9px] text-left'>Description</h3>
-        <Textarea />
-      </div>
-
+    <div className="flex flex-col w-full">
+      <h3 className="font-ibmPlexMono text-[#D1F121] text-[9px] text-left">Description</h3>
+      <Textarea />
+    </div>
   )
 }
 
--- components/MinterPage/MinterPage.tsx
+++ components/MinterPage/MinterPage.tsx
@@ -1,27 +1,22 @@
 'use client'
 
 import MainMediaUpload from './MainMediaUpload'
-import Title from './Title'
-import Animation from './Animation'
-import CreateButtons from './CreateButtons'
-import Description from './Description'
-
 import MinterHeader from './MinterHeader'
-import SaleStrategySwitch from './SaleStrategySwitch'
+import Details from './Details'
+import { useZoraCreateProvider } from '@/providers/ZoraCreateProvider'
 
-const MinterPage = () => (
-  <div className="flex min-h-[100dvh] flex-col items-center justify-center bg-background px-4 py-12 sm:px-6 lg:px-8">
-    <div className=" mt-2 gap-5 mx-auto max-w-md text-center items-center flex flex-col"></div>
-    <div className="gap-5 mx-auto max-w-md text-center items-center flex flex-col">
-      <MinterHeader />
-      <MainMediaUpload />
-      <SaleStrategySwitch />
-      <Title />
-      <Description />
-      <Animation />
-      <CreateButtons />
+const MinterPage = () => {
+  const { name } = useZoraCreateProvider()
+  return (
+    <div className="flex min-h-[100dvh] flex-col items-center justify-center bg-background px-4 py-12 sm:px-6 lg:px-8">
+      <div className=" mt-2 gap-5 mx-auto max-w-md text-center items-center flex flex-col"></div>
+      <div className="gap-5 mx-auto max-w-md text-center items-center flex flex-col">
+        <MinterHeader />
+        <MainMediaUpload />
+        {name && <Details />}
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default MinterPage
