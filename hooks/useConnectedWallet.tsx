7294bf76dc2c8598ade154cf72be2897461b9e2c
Sweets Sweetman <sweetmantech@gmail.com>
2024-08-24 12:39:11 -0300

    I remove unused code in useConnectedWallet.

--- hooks/useConnectedWallet.tsx
+++ hooks/useConnectedWallet.tsx
@@ -3,21 +3,18 @@ import { useMemo } from 'react'
 
 const useConnectedWallet = () => {
   const { wallets } = useWallets()
-  const { ready, authenticated, user } = usePrivy()
-  const isLoggedByEmail = Boolean(user?.email?.address)
+  const { ready, authenticated } = usePrivy()
   const isAuthenticated = ready && authenticated
-  const privyWallet = wallets?.find((wallet) => wallet.walletClientType === 'privy')
   const externalWallets = useMemo(
     () => wallets?.filter((wallet) => wallet.walletClientType !== 'privy'),
     [wallets],
   )
   const externalWallet = externalWallets?.length ? externalWallets[0] : null
-  const wallet = isAuthenticated ? (isLoggedByEmail ? privyWallet : externalWallet) : null
+  const wallet = isAuthenticated ? externalWallet : null
   const connectedWallet = wallet?.address
 
   return {
     connectedWallet,
-    externalWallet,
     wallet,
   }
 }
