2d39692a3a9a362c4d77dfcc71711fbc3e1a94da
Sweets Sweetman <sweetmantech@gmail.com>
2024-08-25 08:53:24 -0300

    I update onCreate redirect to go to prod ENV.

--- hooks/useZoraCreate.tsx
+++ hooks/useZoraCreate.tsx
@@ -71,9 +71,7 @@ const useZoraCreate = () => {
       })
       const address = collectionAddress || (decoded[1] as any).args.newContract
       const tokenId = collectionAddress ? (decoded[3] as any).args.tokenId.toString() : 1
-      await push(
-        `http://newtro-v2-git-test-newtros-projects.vercel.app/collect/bsep:${address}/${tokenId}`,
-      )
+      await push(`http://newtro.xyz/collect/bsep:${address}/${tokenId}`)
       return decoded
     } catch (err) {
       console.error(err)
