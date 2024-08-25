0a7ee2699d4ad15f4fdd42484930e2107a8aa2ca
i9-9 <yvan.vrs@gmail.com>
2024-08-25 10:15:14 -0300

    changes in max-w for mobile

--- components/MinterPage/MinterHeader.tsx
+++ components/MinterPage/MinterHeader.tsx
@@ -1,13 +1,32 @@
 'use client'
 
+import { useEffect, useState } from 'react'
 import { useCollectionProvider } from '@/providers/CollectionProvider'
 
 const MinterHeader = () => {
   const { chain, collectionAddress } = useCollectionProvider()
+  const [isVisible, setIsVisible] = useState(false)
 
+  useEffect(() => {
+    setIsVisible(true)
+  }, [])
+
+  const text = "MINTER";
+  
   return (
     <div>
-      <h1 className="text-[#D1F121] mt-4 text-4xl tracking-tight text-foreground">MINTER</h1>
+      <h1 className="text-[#D1F121] mt-4 text-4xl tracking-tight text-foreground">
+        {text.split('').map((letter, index) => (
+          <span
+            key={index}
+            className={`inline-block opacity-0 transition-opacity duration-1000 ease-in-out ${
+              isVisible ? `opacity-100 animate-fade-in-${index + 1}` : ''
+            }`}
+          >
+            {letter}
+          </span>
+        ))}
+      </h1>
       <p className="text-[#D1F121] text-xs font-light leading-normal text-ock-inverse font-ibmPlexMono">
         Chain: {chain.name || 'Not provided'}
       </p>
--- components/MinterPage/MinterPage.tsx
+++ components/MinterPage/MinterPage.tsx
@@ -6,14 +6,15 @@ import Details from './Details'
 
 const MinterPage = () => (
   <div className="flex min-h-screen flex-col items-center justify-center bg-background px-4 py-12 sm:px-6 lg:px-8 overflow-y-auto">
-    <div className="mt-2 gap-1 mx-auto max-w-md text-center items-center flex flex-col">
+    <div className="mt-2 gap-1 mx-auto text-center items-center flex flex-col">
     </div>
-    <div className="gap-2 mx-auto max-w-md text-center items-center flex flex-col">
+    <div className="gap-2 mx-auto text-center items-center flex flex-col w-full">
       <MinterHeader />
       <MainMediaUpload />
       <Details />
     </div>
   </div>
-)
+);
 
-export default MinterPage
+export default MinterPage;
+
